9958a27042b176c62aef698dbf8e20ed3ad3f535
MichaelWallace30 <Michael.Wallace30@gmail.com>
2016-11-09 15:43:56 -0800

    header packet

---
+++ packetHeader.h
@@ -0,0 +1,74 @@
+
+#include <stdint.h>
+#include <vector>
+#include <cstring>//memcpy
+
+#ifndef _HEADER_PACKET_H
+#define _HEADER_PACKET_H
+
+enum packetType {START, END, SUCCESS, FAILED};
+
+#define HEADER_SIZE 12
+class messageHeader
+{
+public:
+	packetType messageType;
+	uint32_t packet;
+	uint32_t dataLength;	
+
+    messageHeader() :dataLength(0), packet(0), messageType(START)
+    {
+
+    }
+    messageHeader(packetType messageType, uint32_t packet, uint32_t datalength)
+    {
+        this->dataLength = datalength;
+        this->packet = packet;
+        this->messageType = messageType;
+    }
+
+	void deserialize(std::vector<char>* buffer)
+	{
+
+        if (buffer->size() >= HEADER_SIZE)
+        {
+            char arr[HEADER_SIZE] = { 0 };
+            for (int x = 0; x < HEADER_SIZE; x++)
+            {
+                arr[x] = (*buffer)[x];
+            }
+
+            //seralize header
+            memcpy(&this->messageType, arr, 4);
+            memcpy(&this->packet, arr + 4, 4);
+            memcpy(&this->dataLength, arr + 8, 4);
+
+        }
+	}
+
+    std::vector<char>* serialize(std::vector<char>* buffer)
+    {
+
+        //wipe buffer
+        buffer = new std::vector<char>();
+        char arr[HEADER_SIZE] = { 0 };
+
+        
+
+        memcpy(arr, &this->messageType, 4);
+        memcpy(arr + 4, &this->packet, 4);
+        memcpy(arr+ 8, &this->dataLength, 4);
+
+        for (int x = 0; x < HEADER_SIZE; x++)
+        {
+            buffer->push_back(arr[x]);
+        }
+        
+		return buffer;
+	}
+};
+
+
+
+
+#endif
